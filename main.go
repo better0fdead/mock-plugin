ddabe460ae4cc55e5b681b32ee23f31f9163f69d
Терехин Фёдор <terehin.fedor@wb.ru>
2024-08-28 16:53:10 +0300

    temp

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ import (
 //go:embed about.md
 var about []byte
 
+var tags []string
+
+const description = "I do nothing :/"
+
 // Render generates smth.
 func Render(msg []byte, conn net.Conn, n int) error {
 
@@ -21,6 +25,7 @@ func Render(msg []byte, conn net.Conn, n int) error {
 }
 
 func main() {
+	tags = append(tags, "mock", "test", "tag1")
 	// Create a Unix domain socket and listen for incoming connections.
 	socket, err := net.Listen("unix", "./plugin.sock")
 	if err != nil {
@@ -57,9 +62,28 @@ func main() {
 			fmt.Println(buf[:n])
 
 			// Generate smth.
-			err = Render(buf, conn, n)
-			if err != nil {
-				log.Fatal(err)
+			if string(buf[:n]) == "doc" {
+				err = Render(buf, conn, n)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			if string(buf[:n]) == "tag" {
+				for i := range tags {
+					_, err := conn.Write([]byte(tags[i]))
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+
+			if string(buf[:n]) == "desc" {
+				_, err := conn.Write([]byte(description))
+				if err != nil {
+					log.Fatal(err)
+				}
+
 			}
 		}(conn)
 	}
